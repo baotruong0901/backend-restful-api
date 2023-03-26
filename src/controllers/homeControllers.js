19cc2f18a01fe8dc6189fce8d475735f7d433145
bao truong <truongquangbaolut0901@gmail.com>
2023-03-26 11:41:21 +0700

    setup db

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,5 +1,17 @@
+const connection = require('../config/dbConnect')
+
 const getHomePage = (req, res) => {
-    res.send('Hello World!')
+
+    // simple query
+    let users = []
+    connection.query(
+        'SELECT * FROM  Users u',
+        function (err, results, fields) {
+            users = results
+            console.log(">>>results:", results); // results contains rows returned by server
+            res.send(JSON.stringify(users))
+        }
+    );
 }
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const configViewEngine = require('./config/viewEngine')
 const initWebRoutes = require('./routes/web')
+const connection = require('./config/dbConnect')
 const app = express()
 const port = process.env.PORT || 8088
 const hostname = process.env.HOST_NAME
@@ -9,6 +10,15 @@ const hostname = process.env.HOST_NAME
 configViewEngine(app)
 initWebRoutes(app)
 
+
+
+// simple query
+connection.query(
+    'SELECT * FROM  Users u',
+    function (err, results, fields) {
+        console.log(">>>results:", results); // results contains rows returned by server
+    }
+);
 
 app.listen(port, () => {
     console.log(`Backend Nodejs is Running on the port: ${port}`)
